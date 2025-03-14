b84643a9de7d84949b8895ad343d9aa0e2acc184
Saul_Amaris <saul.isusherrero@amaris.com>
2025-03-14 01:00:01 +0100

    Saul_1403

--- src/main/java/tests/InvalidCardPaymentTest.java
+++ src/main/java/tests/InvalidCardPaymentTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -8,12 +9,17 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.HomePage;
+import pages.ResultsPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class HomePageSelectTicketTest {
+public class InvalidCardPaymentTest {
+    //Locators
+    private By assertionResultsPage = By.xpath("//span[@class='label' and contains(text(), 'Selecciona tu viaje')]");
+
     //Variables
     private WebDriver webDriver;
     private HomePage homePage;
+    private ResultsPage resultsPage;
 
     @BeforeMethod
     public void setup() {
@@ -27,6 +33,7 @@ public class HomePageSelectTicketTest {
         webDriver.manage().window().maximize();
         webDriver.get("https://www.renfe.com/es/es"); // URL page
         homePage = new HomePage(webDriver); // Initialization of the Home Page
+        resultsPage = new ResultsPage(webDriver); // Initialization of the Results Page
     }
 
     @Test
@@ -43,10 +50,23 @@ public class HomePageSelectTicketTest {
         homePage.clickSearchTicketButton();
     }
 
-   /**@AfterMethod
+    @Test
+    public void resultsPageInvalidCardPaymentTest() {
+    webDriver.get("https://venta.renfe.com/vol/buscarTrenEnlaces.do?c=_EKaf"); // URL page
+    resultsPage.clickAcceptAllCookiesButton();
+    //Assertion in order to ensure you are inn the correct web page
+    Assert.assertNotEquals(assertionResultsPage, "Selecciona tu viaje");
+    resultsPage.clickFirstAvailableTrain();
+    resultsPage.clickFareApplied();
+    resultsPage.clickSelectionApplied();
+    resultsPage.setRefundCheckboxSelected(true);
+    resultsPage.clickLinkContinueNoRefund();
+    }
+
+    @AfterMethod
     public void tearDown() {
     if (webDriver != null) {
-    webDriver.close(); //Ensures complete browser closure
+    webDriver.quit(); //Close all the browser´s instances
     }
-    }*/
+    }
 }
